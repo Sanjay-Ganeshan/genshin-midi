cfb44b6a39247c4b0a174854e0b0cf6b9268aa1a
Julie Ganeshan <sangan@live.com>
2022-05-03 04:08:30 -0400

    Visualize notes

--- render_notes.py
+++ render_notes.py
@@ -7,6 +7,7 @@ import typing as T
 import pygame
 import pygame.midi
 import numpy as np
+import colorsys
 import time
 pygame.init()
 pygame.midi.init()
@@ -21,7 +22,7 @@ FONT_COLOR = (0, 0, 0)
 MIDDLE_C = 60
 OCTAVE_SEMITONES = 12
 LOWEST_NOTE = MIDDLE_C - OCTAVE_SEMITONES
-HIGHEST_NOTE = MIDDLE_C + OCTAVE_SEMITONES
+HIGHEST_NOTE = MIDDLE_C + OCTAVE_SEMITONES + 7
 
 # Lowest to highest
 KEYBOARD_LETTERS = [
@@ -69,6 +70,35 @@ def midi_pitch_to_keyboard(pitch: int) -> T.Optional[str]:
         return name
 
 
+def rgb_norm_to_rgb(rgb_n: T.Tuple[float, float, float]):
+    r, g, b = rgb_n
+    return (
+        min(255, max(0, int(r * 255))),
+        min(255, max(0, int(g * 255))),
+        min(255, max(0, int(b * 255)))
+    )
+
+def get_note_color(
+    pitch: int
+) -> T.Optional[
+        T.Tuple[
+            T.Tuple[int, int, int],
+            T.Tuple[int, int, int],
+            T.Tuple[int, int, int],
+        ]
+    ]:
+    lower = LOWEST_NOTE - 10
+    higher = HIGHEST_NOTE + 10
+    if pitch < lower or pitch > higher:
+        print(pitch)
+        return None
+    else:
+        norm_offset = (pitch - lower) / (higher - lower)
+        upcoming = colorsys.hsv_to_rgb(norm_offset, 1.0, 1.0)
+        dim = colorsys.hsv_to_rgb(norm_offset, 1.0, 0.4)
+        bright = colorsys.hsv_to_rgb(norm_offset, 0.7, 1.0)
+        return tuple(map(rgb_norm_to_rgb, [upcoming, dim, bright]))
+
 class KeySquare:
     def __init__(self, game: "MIDIRenderer", midi_key: int, norm_xpos: float, norm_ypos: float):
         self.game: "MIDIRenderer" = game
@@ -80,6 +110,12 @@ class KeySquare:
         self.when: T.List[float] = []
         self.norm_xpos: float = norm_xpos
         self.norm_ypos: float = norm_ypos
+        colors = get_note_color(self.midi_key)
+        assert colors is not None
+        self.color: T.Tuple[int, int, int]
+        self.dim_color: T.Tuple[int, int, int]
+        self.bright_color: T.Tuple[int, int, int]
+        self.color, self.dim_color, self.bright_color = colors
 
         m_pitch = midi_pitch_to_name(self.midi_key)
         assert m_pitch is not None, "no sharps or flats"
@@ -91,18 +127,43 @@ class KeySquare:
     def update(self, now: float) -> None:
         while len(self.when) > 0 and self.when[-1] <= now:
             self.when.pop()
-            self.real_toggle()
+            if self.game.recording_plays:
+                self.real_toggle()
 
 
     def draw(self, now: float) -> None:
-        width, height = self.game.norm_size_to_abs(self.game.key_size)
-        left, top = self.game.norm_pos_to_abs((self.norm_xpos, self.norm_ypos), (width, height))
+        # Get common drawing info
+        block_width, block_height = self.game.norm_size_to_abs(self.game.key_size)
         disp = pygame.display.get_surface()
-        pygame.draw.rect(disp, PRESSED_COLOR if (self.is_really_down) else KEY_COLOR, pygame.Rect(left, top, width, height))
+
+        # Draw upcoming notes
+
+        drawing_block = len(self.when) - 1
+        while drawing_block >= 0 and drawing_block < len(self.when):
+            drawing_at = self.when[drawing_block]
+            until_then = drawing_at - now
+            assert until_then >= 0, "Shouldn't be in here"
+            if until_then < self.game.lookahead:
+                norm_until = until_then / self.game.lookahead
+                norm_height_diff = self.game.lookahead_height * norm_until
+                block_norm_ypos = self.norm_ypos - norm_height_diff
+                block_left, block_top = self.game.norm_pos_to_abs((self.norm_xpos, block_norm_ypos), (block_width, block_height))
+                pygame.draw.rect(disp, self.color, pygame.Rect(block_left, block_top, block_width, block_height // 4))
+                drawing_block -= 1
+            else:
+                # Everything else is later
+                break
+
+
+        # Draw the key
+        left, top = self.game.norm_pos_to_abs((self.norm_xpos, self.norm_ypos), (block_width, block_height))
+        pygame.draw.rect(disp, self.dim_color if (self.is_really_down) else self.bright_color, pygame.Rect(left, top, block_width, block_height))
+
+        # Draw the letter on the key
         rendered_text = self.game.font.render(self.keyboard_key_name, True, FONT_COLOR, None)
-        width, height = rendered_text.get_size()
-        left, top = self.game.norm_pos_to_abs((self.norm_xpos, self.norm_ypos), (width, height))
-        disp.blit(rendered_text, pygame.Rect(left, top, width, height), None)
+        txt_width, txt_height = rendered_text.get_size()
+        left, top = self.game.norm_pos_to_abs((self.norm_xpos, self.norm_ypos), (txt_width, txt_height))
+        disp.blit(rendered_text, pygame.Rect(left, top, txt_width, txt_height), None)
 
     def real_down(self):
         if not self.is_really_down:
@@ -126,11 +187,15 @@ class MIDIRenderer():
         self.is_done = False
         self.window_size: T.Tuple[int, int] = pygame.display.get_window_size()
         self.mouse_pos: T.Tuple[int, int] = pygame.mouse.get_pos()
-        self.key_size: float = (0.05, 0.08)
+        self.key_size: float = (0.035, 0.08)
         self.keys: T.Dict[int, KeySquare] = {}
         self.now: float = 0.0
+        self.lookahead: float = 1.0
+        self.lookahead_height: float = 0.42
         self.last_update: T.Optional[float] = None
-        self.transpose_amount = -2
+        self.transpose_amount = 0
+        self.timescale = 0.5
+        self.recording_plays = False
         self.known_files: T.Dict[str, str] = discover_files()
         out_port = pygame.midi.get_default_output_id()
         in_port = pygame.midi.get_default_input_id()
@@ -193,7 +258,7 @@ class MIDIRenderer():
 
         if self.last_update is not None:
             elapsed = nowtime - self.last_update
-            self.now = self.now + elapsed
+            self.now = self.now + (elapsed * self.timescale)
 
         self.last_update = nowtime
 
@@ -285,10 +350,10 @@ def main():
         window_flags = pygame.RESIZABLE | pygame.NOFRAME
     else:
         window_flags = pygame.RESIZABLE
-    pygame.display.set_mode((640, 480), window_flags)
+    pygame.display.set_mode((800, 480), window_flags)
     make_window_transparent()
     game = MIDIRenderer()
-    game.enqueue_file("barricades")
+    game.enqueue_file("my_dearest")
     game.start()
 
 
